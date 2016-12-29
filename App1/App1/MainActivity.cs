512e12968f5ef1b5ca5c61451597255c59f28549
mfacundo94 <mfacundo94@gmail.com>
2016-12-29 14:13:14 -0300

    PantallaLogin

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -22,7 +22,7 @@ namespace App1
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            Button button = FindViewById<Button>(Resource.Id.button1);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
